dd6efdcbdb0ca3571000821fc5f94fdb8657d78e
ismailshaikh8 <31775671+ismailshaikh8@users.noreply.github.com>
2021-11-14 19:52:27 -0500

    Update COVID.sql

--- COVID.sql
+++ COVID.sql
@@ -1,4 +1,4 @@
-#CREATING "coviddeath" TABLE TO INSERT DATA FROM CSV FILE
+#CREATING_coviddeath_TABLE_TO_INSERT_DATA_FROM_CSV_FILE
 CREATE TABLE coviddeaths (
     id INT NOT NULL AUTO_INCREMENT,
     iso_code VARCHAR(255) NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE coviddeaths (
     PRIMARY KEY (id)
 );
 
-#CREATING "covidvaccinated" TABLE TO INSERT DATA FROM CSV FILE
+#CREATING_covidvaccinated_TABLE_TO_INSERT_DATA_FROM_CSV_FILE
 CREATE TABLE covidvaccinated (
     ID INT NOT NULL AUTO_INCREMENT,
     iso_code VARCHAR(255) NOT NULL,
@@ -79,69 +79,74 @@ CREATE TABLE covidvaccinated (
     PRIMARY KEY (ID)
 );
 
-#Looking at Covid Death data
+#Looking_at_Covid_Death_data
 select location AS "Country",date AS "Date",total_cases AS "Total Cases",new_cases AS "New Cases",total_deaths AS "Total Deaths"
 ,population AS "Population"
 from coviddeaths
 order by location,date;
 
-#Percentage of people got infected by COVID-19 in each country by Date
-select location AS "Country",date AS "Date",population AS "Population",total_cases AS "Total Cases"
-,(total_cases/population)*100 AS "Percentage_of_Population_Infected"
-from coviddeaths
-order by location,date;
-
-#Percentage of death due to COVID-19 in each country by Date
-select location AS "Country",date AS "Date",total_cases AS "Total Cases",total_deaths AS "Total Deaths",population AS "Population",
-(total_deaths/total_cases)*100 AS "Death_Percentage(%)"
-from coviddeaths
-order by location,date;
-
-#Total COVID-19 cases by Countries 
-select location AS "Country",SUM(total_cases) AS "Overall Total Cases"
+#Percentage_of_people_got_infected_by_COVID-19_in_each_country
+select location AS "Country",SUM(new_cases) AS "Total Cases"
+,SUM(new_cases/population)*100 AS "Percentage_of_Population_Infected"
 from coviddeaths
 group by location;
 
-#Total Death Count by Countries
-select location AS "Country",SUM(total_deaths) AS "Overall Total Deaths"
+#Percentage_of_death_due_to_COVID-19_in_each_country
+select location AS "Country",SUM(new_cases) AS "Total Cases",SUM(new_deaths) AS "Total Deaths",
+(SUM(new_deaths)/SUM(new_cases))*100 AS "Death_Percentage(%)"
 from coviddeaths
 group by location;
 
-#Total Death Count by continents
-select continent AS "Continent",SUM(total_deaths) AS "Overall_Total_Deaths_Continent"
+#Total_COVID-19_cases_by_Countries 
+select location AS "Country",SUM(new_cases) AS "Overall_Total_Cases"
 from coviddeaths
-where continent is NOT NULL
+where location NOT IN ('World','Upper middle income','High income','Europe','Asia','South America','North America',
+'Lower middle income','European Union')
+group by location 
+order by Overall_Total_Cases desc;
+
+#Total_Death_Count_by_Countries
+select location AS "Country",SUM(new_deaths) AS "Overall_Total_Deaths"
+from coviddeaths
+where location NOT IN ('World','Upper middle income','High income','Europe','Asia','South America','North America',
+'Lower middle income','European Union')
+group by location
+order by Overall_Total_Deaths desc;
+
+#Total_Death_Count_by_continents
+select continent AS "Continent",SUM(new_deaths) AS "Overall_Total_Deaths_Continent"
+from coviddeaths
+where continent IS NOT NULL
 group by continent
 order by Overall_Total_Deaths_Continent desc;
 
-#Highest Death Count in a Single day by Countries 
-select location AS "Country",MAX(CAST(total_deaths AS SIGNED)) AS "Highest_Death" 
+#Highest_Death_Count_in_a_Single_day_by_Countries 
+select location AS "Country",MAX(CAST(new_deaths AS SIGNED)) AS "Highest_Death" 
 from coviddeaths
 Where location NOT IN('World','Upper middle income','High income','Europe','Asia','South America','North America',
 'Lower middle income','European Union')
 group by location
 order by Highest_Death desc;
 
-#Highest Death Count in a Single day by Continents
-select continent AS "Continent",MAX(CAST(total_deaths AS SIGNED)) AS "Highest_Death_Continent"
+#Highest_Death_Count_in_a_Single_day_by_Continents
+select continent AS "Continent",MAX(CAST(new_deaths AS SIGNED)) AS "Highest_Death_Continent"
 from coviddeaths
 where continent is NOT NULL
 group by continent
 order by Highest_Death_Continent desc;
 
-#Highest Infection Rate in a single day by Countries 
-select location AS "Country",population AS "Population",MAX(total_cases) AS "Max Cases in a Day",((MAX(total_cases)/population)*100)AS "Highest_Infection_Rate"
+#Highest_Infection_Rate_in_a_single_day_by_Countries 
+select location AS "Country",population AS "Population",MAX(new_cases) AS "Max Cases in a Day",((MAX(new_cases)/population)*100)AS "Highest_Infection_Rate"
 from coviddeaths
-group by location,population
-order by Highest_Infection_Rate desc;
+group by location,population;
 
-#Total New Cases and Total New Deaths by countries
-select location AS "Country", SUM(new_cases) AS "Total_New_Cases_By_Countries", SUM(new_deaths) AS "Total_New_Deaths_By_Countries"
+#Total_New_Cases_and_Total_New_Deaths_by_countries
+select location AS "Country", SUM(new_cases) AS "Total_Cases_By_Countries", SUM(new_deaths) AS "Total_Deaths_By_Countries"
 ,((SUM(new_deaths)/SUM(new_cases))*100) AS "New_Deaths(%)"
 from coviddeaths
 group by location; 
 
-#Joining coviddeaths and covidvaccinated Tables
+#Joining_coviddeaths_and_covidvaccinated_Tables
 #Method 1
 select *
 from covidvaccinated JOIN coviddeaths ON
@@ -153,14 +158,14 @@ select *
 from covidvaccinated JOIN coviddeaths ON
 covidvaccinated.ID = coviddeaths.id;
 
-#Total number of vaccinated people in each Country
+#Total_number_of_vaccinated_people_in_each_Country
 select coviddeaths.location AS "Country",SUM(covidvaccinated.new_vaccinations) AS "Total_Vaccinations_By_Countries"
 from covidvaccinated JOIN coviddeaths ON 
 covidvaccinated.location = coviddeaths.location AND 
 covidvaccinated.date = coviddeaths.date
 group by coviddeaths.location;
 
-#Total Number of vaccinated people in each country on each date
+#Total_Number_of_vaccinated_people_in_each_country_on_each_date
 select coviddeaths.location AS "Country",coviddeaths.date AS "Date",coviddeaths.population AS "Population",
 covidvaccinated.new_vaccinations AS "New Vaccinations",SUM(covidvaccinated.new_vaccinations) OVER 
 (PARTITION BY coviddeaths.location ORDER BY coviddeaths.location,coviddeaths.date) AS "Total_No_Of_Vaccinated_People_on_Each_Date"
@@ -168,7 +173,7 @@ from covidvaccinated JOIN coviddeaths ON
 covidvaccinated.location = coviddeaths.location AND
 covidvaccinated.date = coviddeaths.date;
 
-#Percentage of Total Number of vaccinated people in each country on each date
+#Percentage_of_Total_Number_of_vaccinated_people_in_each_country_on_each_date
 WITH VaccinationPercent (Location,Date,Population,New_Vaccinations,Total_No_Of_Vaccinated_People_on_Each_Date) 
 AS
 (
